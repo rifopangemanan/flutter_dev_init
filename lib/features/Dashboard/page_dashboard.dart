732eb75ee801937d9b46f80beb46ee76fe2201ca
Yolanda Terok <yolandaterok@Yolandas-MacBook-Air.local>
2026-02-03 11:21:00 +0800

    init commit

---
+++ lib/features/Dashboard/page_dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_card/features/Dashboard/widget_page.dart';
+
+class PageDashboard extends StatelessWidget {
+  const PageDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(  
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('Dashboard'),
+      ), 
+      body: const WidgetPage(),
+    );  
+  }
+}
